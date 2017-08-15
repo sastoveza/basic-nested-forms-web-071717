ba025db7e0f2677f409dfe8f9d6c41ec945dd86c
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-15 16:49:47 -0400

    done

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,13 +1,16 @@
 class PeopleController < ApplicationController
-  def new
+
+def new
     @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
 
-  def create    
-    Person.create(person_params)
+  def create
+    person = Person.create(person_params)
     redirect_to people_path
   end
-
+  
   def index
     @people = Person.all
   end
